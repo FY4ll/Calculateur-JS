36feb405fa8b8e9e4627b4fbe6130333d6986b13
TCwolfTC <fadyallemann@gmail.com>
2022-12-05 17:44:10 +0100

    Made the table system

--- main.js
+++ main.js
@@ -1,28 +1,99 @@
-let noteInput = document.querySelector('#note');
-let descInput = document.querySelector('#desc');
-let dateInput = document.querySelector('#date');
-let tableGrades = []
+let Notes = {
+    ecp: {
+        semestre1: [],
+        mod1: [],
+        moy: [],
+    },
+    cie: {
+        semestre1: [],
+        mod1: [],
+        moy: [],
+    },
+    cdb: {
+        mathSemestre1: [],
+        mathSemestre2: [],
+        mathSemestre3: [],
+        anglaisSemestre1: [],
+        anglaisSemestre2: [],
+        anglaisSemestre3: [],
+        anglaisSemestre4: [],
+        anglaisSemestre5: [],
+        moy: [],
+    },
+    cg: {
+        cultGSemestre1: [],
+        cultGSemestre2: [],
+        cultGSemestre3: [],
+        cultGSemestre4: [],
+        cultGSemestre5: [],
+        cultGSemestre6: [],
+        cultGSemestre7: [],
+        cultGSemestre8: [],
+        moy: [],
+    },
+    tpi: {
+        semestre1: [],
+        mod1: [],
+        moy: [],
+    },
+};
 
-document.querySelector('#add').addEventListener('click', () => {
-    let sum = 0;
-    let note = parseFloat(noteInput.value);
-    tableGrades.push(note);
-    let desc = descInput.value;
-    let date = dateInput.value;
+let branche;
+let module;
+let date;
+let btnAdd = document.querySelector('#add');
+let table = document.querySelector('table');
+let noteInput = document.getElementById('note')
+let descInput = document.querySelector('#desc')
+let dateInput = document.querySelector('#date')
+let brancheButtons = document.getElementsByClassName('branchselector')
+console.log(branche)
 
-    for (const grade of tableGrades) {
-        sum += grade;
-    }
-    let moyenne = sum / tableGrades.length;
-    let template = `
+
+for (const branchebutton of brancheButtons) {
+    branchebutton.addEventListener("click", function () {
+        branche = branchebutton.value
+        console.log(branche);
+    });
+}
+// let semestreButtons = document.getElementsByName('semestre')
+// console.log(semestreButtons)
+// for (const semestrebutton of semestreButtons) {
+//     semestrebutton.addEventListener("click", function () {
+//         let value = semestrebutton.value
+//         console.log(value);
+//     });
+// };
+let mathanglaisButtons = document.getElementsByName('cbma')
+console.log(mathanglaisButtons)
+for (const mathanglaisbutton of mathanglaisButtons) {
+    mathanglaisbutton.addEventListener("click", function () {
+        let value = mathanglaisbutton.value;
+        console.log(value);
+    });
+};
+btnAdd.addEventListener('click', function () {
+    if (branche == undefined) {
+        alert("vous devez choisir une branche");
+    } else {
+        let note = noteInput.value;
+        let desc = descInput.value;
+        let date = dateInput.value;
+        let template = `
                 <tr>
                     <td>${note}</td>
                     <td>${desc}</td>
                     <td>${date}</td>
-                    <td>${moyenne}</td>
                 </tr>
-                `;
-    document.querySelector('table').innerHTML += template;
+                    `
+        table.innerHTML += template;
+        console.log(branche);
+        Notes[branche].semestre1.push(note);
+        console.log(Notes[branche].semestre1);
+    }
 });
 
-    //.toFixed(1)
+function average() {
+
+}
+
